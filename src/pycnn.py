f1ccb6730aead73bcca3c21a2691429abc9752e7
mike pekala <mpekala@umd.edu>
2015-10-22 17:08:12 -0400

    added some new functions

--- src/pycnn.py
+++ src/pycnn.py
@@ -8,9 +8,13 @@ __copyright__ = "Copyright 2015, JHU/APL"
 __license__ = "Apache 2.0"
 
 
+import sys, re
+
+import numpy as np
 
 
-def _print_net(net):
+
+def print_net(net):
     """Shows some info re. a Caffe network to stdout
 
     net : a PyCaffe Net object.
@@ -23,10 +27,71 @@ def _print_net(net):
         for jj,blob in enumerate(layer.blobs):
             print("    blob %d has size %s" % (jj, str(blob.data.shape)))
 
-            
-#-------------------------------------------------------------------------------
-# Functions for training a CNN
-#-------------------------------------------------------------------------------
+
+
+def infer_data_dimensions(netFn):
+    """Determine the size of the Caffe input data tensor.
+
+    There may be a cleaner way to do this through the pycaffe API (e.g. via the
+    network parameters protobuf object).
+    """
+    with open(netFn, 'r') as f:
+        contents = "".join(f.readlines())
+
+    dimNames = ['batch_size', 'channels', 'height', 'width']
+    dimensions = np.zeros((4,), dtype=np.int32)
+
+    for ii, dn in enumerate(dimNames):
+        pat = r'%s:\s*(\d+)' % dn
+        mo = re.search(pat, contents)
+        if mo is None:
+            raise RuntimeError('Unable to extract "%s" from network file "%s"' % (dn, netFn))
+        dimensions[ii] = int(mo.groups()[0])
+        
+    return dimensions
+
+
+
+def metrics(Y, Yhat, display=False): 
+    """
+    PARAMETERS:
+      Y    :  a numpy array of true class labels (vector)
+      Yhat : a numpy array of estimated class labels (same size as Y)
+
+    o Assumes any class label <0 should be ignored in the analysis.
+    o Assumes all non-negative class labels are contiguous and start at 0.
+      (so for binary classification, the class labels are {0,1})
+    """
+
+    # create a confusion matrix
+    # yAll is all *non-negative* class labels in Y
+    yAll = np.unique(Y);  yAll = yAll[yAll >= 0] 
+    C = np.zeros((yAll.size, yAll.size))
+    for yi in yAll:
+        est = Yhat[Y==yi]
+        for jj in yAll:
+            C[yi,jj] = np.sum(est==jj)
+
+    # works for arbitrary # of classes
+    acc = 1.0*np.sum(Yhat[Y>=0] == Y[Y>=0]) / np.sum(Y>=0)
+
+    # binary classification metrics (only for classes {0,1})
+    nTruePos = 1.0*np.sum((Y==1) & (Yhat==1))
+    precision = nTruePos / np.sum(Yhat==1)
+    recall = nTruePos / np.sum(Y==1)
+    f1 = 2.0*(precision*recall) / (precision+recall)
+
+    if display: 
+        for ii in range(C.shape[0]): 
+            print('  class=%d    %s' % (ii, C[ii,:]))
+        print('  accuracy:  %0.3f' % (acc))
+        print('  precision: %0.3f' % (precision))
+        print('  recall:    %0.3f' % (recall))
+        print('  f1:        %0.3f' % (f1))
+
+    return C, acc, precision, recall, f1
+
+
 
 class TrainInfo:
     """
@@ -75,125 +140,4 @@ class TrainInfo:
 
         # XXX: weight decay
         # XXX: layer-specific weights
-
-
-
-def train_one_epoch(solver, X, Y, 
-        trainInfo, 
-        batchDim,
-        outDir='./', 
-        omitLabels=[], 
-        data_augment=None):
-    """ Trains a CNN for a single epoch.
-
-    You can call multiple times - just be sure to pass in the latest
-    trainInfo object each time.
-
-    PARAMETERS:
-      solver    : a PyCaffe solver object
-      X         : a data volume/tensor with dimensions (#slices, height, width)
-      Y         : a labels tensor with same size as X
-      trainInfo : a TrainInfo object (will be modified by this function!!)
-      batchDim  : the tuple (#classes, minibatchSize, height, width)
-      outDir    : output directory (e.g. for model snapshots)
-      omitLabels : class labels to skip during training (or [] for none)
-      data_agument : synthetic data augmentation function
-
-    """
-
-    # Pre-allocate some variables & storage.
-    #
-    tileRadius = int(batchDim[2]/2)
-    Xi = np.zeros(batchDim, dtype=np.float32)
-    yi = np.zeros((batchDim[0],), dtype=np.float32)
-    yMax = np.max(Y).astype(np.int32)
-    
-    tic = time.time()
-    it = emlib.stratified_interior_pixel_generator(Y, tileRadius, batchDim[0], omitLabels=omitLabels) 
-
-    for Idx, epochPct in it: 
-        # Map the indices Idx -> tiles Xi and labels yi 
-        # 
-        # Note: if Idx.shape[0] < batchDim[0] (last iteration of an epoch) 
-        # a few examples from the previous minibatch will be "recycled" here. 
-        # This is intentional (to keep batch sizes consistent even if data 
-        # set size is not a multiple of the minibatch size). 
-        # 
-        for jj in range(Idx.shape[0]): 
-            a = Idx[jj,1] - tileRadius 
-            b = Idx[jj,1] + tileRadius + 1 
-            c = Idx[jj,2] - tileRadius 
-            d = Idx[jj,2] + tileRadius + 1 
-            Xi[jj, 0, :, :] = X[ Idx[jj,0], a:b, c:d ]
-            yi[jj] = Y[ Idx[jj,0], Idx[jj,1], Idx[jj,2] ] 
-
-        # label-preserving data transformation (synthetic data generation)
-        if data_augment is not None:
-            Xi = data_augment(Xi)
-
-        # convert labels to a 4d tensor
-        yiTensor = np.ascontiguousarray(yi[:, np.newaxis, np.newaxis, np.newaxis])
-
-        assert(not np.any(np.isnan(Xi)))
-        assert(not np.any(np.isnan(yi)))
-
-        #----------------------------------------
-        # one forward/backward pass and update weights
-        #----------------------------------------
-        _tmp = time.time()
-        solver.net.set_input_arrays(Xi, yiTensor)
-        out = solver.net.forward()
-        assert(np.all(solver.net.blobs['data'].data == Xi))
-        assert(np.all(solver.net.blobs['label'].data == yiTensor))
-        solver.net.backward()
-
-        # SGD with momentum
-        for lIdx, layer in enumerate(solver.net.layers):
-            for bIdx, blob in enumerate(layer.blobs): 
-                if np.any(np.isnan(blob.diff)): 
-                    raise RuntimeError("NaN detected in gradient of layer %d" % lIdx)
-                key = (lIdx, bIdx)
-                V = trainInfo.V.get(key, 0.0)
-                Vnext = (trainInfo.mu * V) - (trainInfo.alpha * blob.diff)
-                blob.data[...] += Vnext
-                trainInfo.V[key] = Vnext
-
-        # (try to) extract some useful info from the net
-        loss = out.get('loss', None)
-        acc = out.get('acc', None)
-
-        # update run statistics
-        trainInfo.cnnTime += time.time() - _tmp
-        trainInfo.iter += 1
-        trainInfo.netTime += (time.time() - tic)
-        tic = time.time()
-
-        #----------------------------------------
-        # Some events occur on regular intervals.
-        # Address these here.
-        #----------------------------------------
-        if (trainInfo.iter % trainInfo.param.snapshot) == 0:
-            fn = os.path.join(outDir, 'iter_%06d.caffemodel' % trainInfo.iter)
-            solver.net.save(str(fn))
-
-        if trainInfo.isModeStep and ((trainInfo.iter % trainInfo.param.stepsize) ==0):
-            trainInfo.alpha *= trainInfo.gamma
-
-        if (trainInfo.iter % trainInfo.param.display) == 1: 
-            print "[emCNN]: completed iteration %d of %d (epoch=%0.2f);" % (trainInfo.iter, trainInfo.param.max_iter, trainInfo.epoch+epochPct)
-            print "[emCNN]:     %0.2f min elapsed (%0.2f CNN min)" % (trainInfo.netTime/60., trainInfo.cnnTime/60.)
-            print "[emCNN]:     alpha=%0.4e" % (trainInfo.alpha)
-            if loss: 
-                print "[emCNN]:     loss=%0.3f" % loss
-            if acc: 
-                print "[emCNN]:     accuracy (train volume)=%0.3f" % acc
-            sys.stdout.flush()
- 
-        if trainInfo.iter >= trainInfo.param.max_iter:
-            break  # we hit max_iter on a non-epoch boundary...all done.
-                
-    # all finished with this epoch
-    print "[emCNN]:    epoch complete."
-    sys.stdout.flush()
-    return loss, acc
 
