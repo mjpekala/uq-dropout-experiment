796867d62e86d96166a4deb52646c13a6c74ba15
Pekala <pekalmj1@pekalmj1-ml1.jhuapl.edu>
2015-10-22 14:40:45 -0400

    moved some code into pycnn

--- src/cifar_experiment.py
+++ src/cifar_experiment.py
@@ -1,5 +1,4 @@
 """ 
-
 """
 
 
@@ -45,7 +44,6 @@ def _get_args():
 
 
 #-------------------------------------------------------------------------------
-# some helper functions
 #-------------------------------------------------------------------------------
 
 
@@ -88,7 +86,9 @@ def _read_cifar10_binary(fn):
 
 
 def _minibatch_generator(X, y, nBatch=100, yOmit=[], randomOrder=True):
-    """
+    """Generator that returns subsets (mini-batches) of the provided data set.
+
+    We could also add data augmentation here, but for not this isn't necessary.
     """
 
     # pare down to indices for labels that we do care about
@@ -114,314 +114,6 @@ def _minibatch_generator(X, y, nBatch=100, yOmit=[], randomOrder=True):
 
         yield Xi, yi, nThisBatch
         
-
-    
-    
-
-def _print_net(net):
-    """Shows some info re. a Caffe network to stdout
-    """
-    for name, blobs in net.params.iteritems():
-        for bIdx, b in enumerate(blobs):
-            print("  %s[%d] : %s" % (name, bIdx, b.data.shape))
-    for ii,layer in enumerate(net.layers):
-        print("  layer %d: %s" % (ii, layer.type))
-        for jj,blob in enumerate(layer.blobs):
-            print("    blob %d has size %s" % (jj, str(blob.data.shape)))
-
-
-            
-def _load_data(xName, yName, tileRadius, onlySlices, omitLabels=None):
-    """Loads data sets and does basic preprocessing.
-    """
-    X = emlib.load_cube(xName, np.float32)
-
-    # usually we expect fewer slices in Z than pixels in X or Y.
-    # Make sure the dimensions look ok before proceeding.
-    assert(X.shape[0] < X.shape[1])
-    assert(X.shape[0] < X.shape[2])
-
-    if onlySlices: 
-        X = X[onlySlices,:,:] 
-    print('[emCNN]:    data shape: %s' % str(X.shape))
-
-    X = emlib.mirror_edges(X, tileRadius)
-
-    # Scale data to live in [0 1].
-    # *** ASSUMPTION *** original data is in [0 255]
-    if np.max(X) > 1:
-        X = X / 255.
-    print('[emCNN]:    data min/max: %0.2f / %0.2f' % (np.min(X), np.max(X)))
-
-    # Also obtain labels file (if provided - e.g. in deploy mode
-    # we may not have labels...)
-    if yName: 
-        Y = emlib.load_cube(yName, np.float32)
-
-        if onlySlices: 
-            Y = Y[onlySlices,:,:] 
-        print('[emCNN]:    labels shape: %s' % str(Y.shape))
-
-        # ** ASSUMPTION **: Special case code for membrane detection / ISBI volume
-        yAll = np.unique(Y)
-        yAll.sort()
-        if (len(yAll) == 2) and (yAll[0] == 0) and (yAll[1] == 255):
-            print('[emCNN]:    ISBI-style labels detected.  converting 0->1, 255->0')
-            Y[Y==0] = 1;      #  membrane
-            Y[Y==255] = 0;    #  non-membrane
-
-        # Labels must be natural numbers (contiguous integers starting at 0)
-        # because they are mapped to indices at the output of the network.
-        # This next bit of code remaps the native y values to these indices.
-        omitLabels, pctOmitted = _omit_labels(Y, omitLabels)
-        Y = emlib.fix_class_labels(Y, omitLabels).astype(np.int32)
-
-        print('[emCNN]:    yAll is %s' % str(np.unique(Y)))
-        print('[emCNN]:    will use %0.2f%% of training volume' % (100.0 - pctOmitted))
-
-        Y = emlib.mirror_edges(Y, tileRadius)
-
-        return X, Y
-    else:
-        return X
-
-
-
-#-------------------------------------------------------------------------------
-# Functions for training a CNN
-#-------------------------------------------------------------------------------
-
-class TrainInfo:
-    """
-    Used to store/update CNN parameters over time.
-    """
-
-    def __init__(self, solverParam):
-        self.param = solverParam
-
-        self.isModeStep = (solverParam.lr_policy == u'step')
-
-        # This code only supports some learning strategies
-        if not self.isModeStep:
-            raise ValueError('Sorry - I only support step policy at this time')
-
-        if (solverParam.solver_type != solverParam.SolverType.Value('SGD')):
-            raise ValueError('Sorry - I only support SGD at this time')
-
-        # keeps track of the current mini-batch iteration and how
-        # long the processing has taken so far
-        self.iter = 0
-        self.epoch = 0
-        self.cnnTime = 0
-        self.netTime = 0
-
-        #--------------------------------------------------
-        # SGD parameters.  SGD with momentum is of the form:
-        #
-        #    V_{t+1} = \mu V_t - \alpha \nablaL(W_t)
-        #    W_{t+1} = W_t + V_{t+1}
-        #
-        # where W are the weights and V the previous update.
-        # Ref: http://caffe.berkeleyvision.org/tutorial/solver.html
-        #
-        #--------------------------------------------------
-        self.alpha = solverParam.base_lr  # := learning rate
-        self.mu = solverParam.momentum    # := momentum
-        self.gamma = solverParam.gamma    # := step factor
-        self.V = {}                       # := previous values (for momentum)
-
-        assert(self.alpha > 0)
-        assert(self.gamma > 0)
-        assert(self.mu >= 0)
-
-
-        # XXX: weight decay
-        # XXX: layer-specific weights
-
-
-
-def _xform_minibatch(X, rotate=False, prob=0.5):
-    """Synthetic data augmentation for one mini-batch.
-    
-    Parameters: 
-       X := Mini-batch data (# slices, # channels, rows, colums) 
-       
-       rotate := a boolean; when true, will rotate the mini-batch X
-                 by some angle in [0, 2*pi)
-
-       prob := probability of applying any given operation
-
-    Note: for some reason, the implementation of row and column reversals, e.g.
-               X[:,:,::-1,:]
-          break PyCaffe.  Numpy must be doing something under the hood 
-          (e.g. changing from C order to Fortran order) to implement this 
-          efficiently which is incompatible w/ PyCaffe.  
-          Hence the explicit construction of X2 with order 'C' in the
-          nested functions below.
-    """
-
-    def fliplr(X):
-        X2 = np.zeros(X.shape, dtype=np.float32, order='C') 
-        X2[:,:,:,:] = X[:,:,::-1,:]
-        return X2
-
-    def flipud(X):
-        X2 = np.zeros(X.shape, dtype=np.float32, order='C') 
-        X2[:,:,:,:] = X[:,:,:,::-1]
-        return X2
-
-    def transpose(X):
-        X2 = np.zeros(X.shape, dtype=np.float32, order='C') 
-        X2[:,:,:,:] = np.transpose(X, [0, 1, 3, 2])
-        return X2
-
-    def identity(X): return X
-
-    prob = min(1.0, prob)
-    prob = max(0.0, prob)
-
-    if rotate: 
-        # rotation by an arbitrary angle 
-        # Note: this is very slow!!
-        # Note: this should probably be implemented at a higher level
-        #       (than the individual mini-batch) so we can incorporate
-        #       context rather than filling in pixels.
-        angle = np.random.rand() * 360.0 
-        fillColor = np.max(X) 
-        X2 = scipy.ndimage.rotate(X, angle, axes=(2,3), reshape=False, cval=fillColor)
-    else:
-        ops = []
-        ops.append( fliplr if np.random.rand() < prob else identity )
-        ops.append( flipud if np.random.rand() < prob else identity )
-        ops.append( transpose if np.random.rand() < prob else identity )
-        shuffle(ops)
-        X2 = ops[0](X)
-        for op in ops[1:]:
-            X2 = op(X2)
-
-    return X2
-
-
-
-def train_one_epoch(solver, X, Y, 
-        trainInfo, 
-        batchDim,
-        outDir='./', 
-        omitLabels=[], 
-        data_augment=None):
-    """ Trains a CNN for a single epoch.
-
-    You can call multiple times - just be sure to pass in the latest
-    trainInfo object each time.
-
-    PARAMETERS:
-      solver    : a PyCaffe solver object
-      X         : a data volume/tensor with dimensions (#slices, height, width)
-      Y         : a labels tensor with same size as X
-      trainInfo : a TrainInfo object (will be modified by this function!!)
-      batchDim  : the tuple (#classes, minibatchSize, height, width)
-      outDir    : output directory (e.g. for model snapshots)
-      omitLabels : class labels to skip during training (or [] for none)
-      data_agument : synthetic data augmentation function
-
-    """
-
-    # Pre-allocate some variables & storage.
-    #
-    tileRadius = int(batchDim[2]/2)
-    Xi = np.zeros(batchDim, dtype=np.float32)
-    yi = np.zeros((batchDim[0],), dtype=np.float32)
-    yMax = np.max(Y).astype(np.int32)
-    
-    tic = time.time()
-    it = emlib.stratified_interior_pixel_generator(Y, tileRadius, batchDim[0], omitLabels=omitLabels) 
-
-    for Idx, epochPct in it: 
-        # Map the indices Idx -> tiles Xi and labels yi 
-        # 
-        # Note: if Idx.shape[0] < batchDim[0] (last iteration of an epoch) 
-        # a few examples from the previous minibatch will be "recycled" here. 
-        # This is intentional (to keep batch sizes consistent even if data 
-        # set size is not a multiple of the minibatch size). 
-        # 
-        for jj in range(Idx.shape[0]): 
-            a = Idx[jj,1] - tileRadius 
-            b = Idx[jj,1] + tileRadius + 1 
-            c = Idx[jj,2] - tileRadius 
-            d = Idx[jj,2] + tileRadius + 1 
-            Xi[jj, 0, :, :] = X[ Idx[jj,0], a:b, c:d ]
-            yi[jj] = Y[ Idx[jj,0], Idx[jj,1], Idx[jj,2] ] 
-
-        # label-preserving data transformation (synthetic data generation)
-        if data_augment is not None:
-            Xi = data_augment(Xi)
-
-        # convert labels to a 4d tensor
-        yiTensor = np.ascontiguousarray(yi[:, np.newaxis, np.newaxis, np.newaxis])
-
-        assert(not np.any(np.isnan(Xi)))
-        assert(not np.any(np.isnan(yi)))
-
-        #----------------------------------------
-        # one forward/backward pass and update weights
-        #----------------------------------------
-        _tmp = time.time()
-        solver.net.set_input_arrays(Xi, yiTensor)
-        out = solver.net.forward()
-        assert(np.all(solver.net.blobs['data'].data == Xi))
-        assert(np.all(solver.net.blobs['label'].data == yiTensor))
-        solver.net.backward()
-
-        # SGD with momentum
-        for lIdx, layer in enumerate(solver.net.layers):
-            for bIdx, blob in enumerate(layer.blobs): 
-                if np.any(np.isnan(blob.diff)): 
-                    raise RuntimeError("NaN detected in gradient of layer %d" % lIdx)
-                key = (lIdx, bIdx)
-                V = trainInfo.V.get(key, 0.0)
-                Vnext = (trainInfo.mu * V) - (trainInfo.alpha * blob.diff)
-                blob.data[...] += Vnext
-                trainInfo.V[key] = Vnext
-
-        # (try to) extract some useful info from the net
-        loss = out.get('loss', None)
-        acc = out.get('acc', None)
-
-        # update run statistics
-        trainInfo.cnnTime += time.time() - _tmp
-        trainInfo.iter += 1
-        trainInfo.netTime += (time.time() - tic)
-        tic = time.time()
-
-        #----------------------------------------
-        # Some events occur on regular intervals.
-        # Address these here.
-        #----------------------------------------
-        if (trainInfo.iter % trainInfo.param.snapshot) == 0:
-            fn = os.path.join(outDir, 'iter_%06d.caffemodel' % trainInfo.iter)
-            solver.net.save(str(fn))
-
-        if trainInfo.isModeStep and ((trainInfo.iter % trainInfo.param.stepsize) ==0):
-            trainInfo.alpha *= trainInfo.gamma
-
-        if (trainInfo.iter % trainInfo.param.display) == 1: 
-            print "[emCNN]: completed iteration %d of %d (epoch=%0.2f);" % (trainInfo.iter, trainInfo.param.max_iter, trainInfo.epoch+epochPct)
-            print "[emCNN]:     %0.2f min elapsed (%0.2f CNN min)" % (trainInfo.netTime/60., trainInfo.cnnTime/60.)
-            print "[emCNN]:     alpha=%0.4e" % (trainInfo.alpha)
-            if loss: 
-                print "[emCNN]:     loss=%0.3f" % loss
-            if acc: 
-                print "[emCNN]:     accuracy (train volume)=%0.3f" % acc
-            sys.stdout.flush()
- 
-        if trainInfo.iter >= trainInfo.param.max_iter:
-            break  # we hit max_iter on a non-epoch boundary...all done.
-                
-    # all finished with this epoch
-    print "[emCNN]:    epoch complete."
-    sys.stdout.flush()
-    return loss, acc
-
 
 
 def _train_network(args):
@@ -731,16 +423,16 @@ if __name__ == "__main__":
     args = _get_args()
     print(args)
 
-    #import caffe
-    #from caffe.proto import caffe_pb2
-    #from google.protobuf import text_format
+    import caffe
+    from caffe.proto import caffe_pb2
+    from google.protobuf import text_format
 
-    ## specify CPU or GPU
-    #if args.gpu >= 0:
-    #    caffe.set_mode_gpu()
-    #    caffe.set_device(args.gpu)
-    #else:
-    #    caffe.set_mode_cpu()
+    # specify CPU or GPU
+    if args.gpu >= 0:
+        caffe.set_mode_gpu()
+        caffe.set_device(args.gpu)
+    else:
+        caffe.set_mode_cpu()
 
     
     # Do either training or deployment
