29507a18a67acd0dd7b969483e0a6f3133d1703a
Pekala <pekalmj1@pekalmj1-ml1.jhuapl.edu>
2015-10-23 14:53:34 -0400

    updated plots

--- src/postproc.m
+++ src/postproc.m
@@ -2,7 +2,7 @@
 
 load('Deploy.mat');  % creates 'y', 'Prob'
 
-classes = {'airplane', 'automobile', 'bird', 'cat', 'deer', ...
+classes = {'plane', 'auto', 'bird', 'cat', 'deer', ...
            'dog', 'frog', 'horse', 'ship', 'truck'};
 
 Mu = mean(Prob,3);
@@ -16,6 +16,25 @@ acc = 100*sum(yHat == y) / numel(y);
 C = confusionmat(double(y), yHat)
 figure; imagesc(C);
 set(gca, 'YTick', 1:10, 'YTickLabel', classes);
+set(gca, 'XTick', 1:10, 'XTickLabel', classes);
 title(sprintf('CIFAR-10; acc=%0.2f', acc));
 xlabel('predicted class');
 ylabel('true class');
+
+
+for ii = 1:20
+    Xi = squeeze(Prob(ii,:,:));   %  (#classes x #samples)
+    Xi = Xi';                     %  -> rows-as-examples
+    Cov = cov(Xi);
+  
+    figure; imagesc(Xi);
+    xlabel('sample'); 
+    set(gca, 'XTick', 1:10, 'XTickLabel', classes);
+    title(sprintf('Example %d; class=%s', ii, classes{y(ii)+1}));
+    colorbar;
+    
+    figure; imagesc(Cov);
+    set(gca, 'YTick', 1:10, 'YTickLabel', classes);
+    set(gca, 'XTick', 1:10, 'XTickLabel', classes);
+    title('covariance');
+end
