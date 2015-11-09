15d65feff26c53c828f521f3ec9f92f7e9aeaa0b
Pekala <pekalmj1@pekalmj1-ml1.jhuapl.edu>
2015-11-09 08:26:04 -0500

    updated

--- src/postproc.m
+++ src/postproc.m
@@ -1,4 +1,6 @@
-% POSTPROC  Evaluates result of a CIFAR-10 experiment.
+% POSTPROC_CIFAR10  Evaluates result of a CIFAR-10 experiment.
+%
+%
 
 load('Deploy.mat');  % creates 'X', 'y', 'Prob'
 X = permute(X, [3, 4, 2, 1]);
@@ -9,10 +11,10 @@ classes = {'plane', 'auto', 'bird', 'cat', 'deer', ...
            'dog', 'frog', 'horse', 'ship', 'truck'};
 
 Mu = mean(Prob,3);
-[~,yHat] = max(Mu,[],2);
+[~,yHatOneBased] = max(Mu,[],2);
 
 % correct for fact that matlab is 1-indexed 
-yHat = yHat - 1;
+yHat = yHatOneBased - 1;
 
 acc = 100*sum(yHat == y) / numel(y);
 
@@ -25,14 +27,56 @@ ylabel('true class');
 title(sprintf('CIFAR-10 confusion matrix; acc=%0.2f', acc));
 
 
-muMax = max(Mu, [], 2);
+% calculate some measure of uncertainty for each prediction
+variance = zeros(size(yHat));
+for ii = 1:size(Prob,1)
+    Pii = squeeze(Prob(ii,:,:));
+    samps = Pii(yHatOneBased(ii),:);
+    variance(ii) = var(samps);
+end
+
+muGap = zeros(size(yHat));
+for ii = 1:size(Mu,1)
+    ordered = sort(Mu(ii,:), 'descend');
+    muGap(ii) = ordered(1) - ordered(2);
+end
+
+
+
+
+%-------------------------------------------------------------------------------
+% Visualize different ways of characterizing uncertainty
+%-------------------------------------------------------------------------------
+% compare distribution of max(mu) for correct and incorrect examples.
+figure;
+boxplot(1 - max(Mu,[],2), yHat==y);
+set(gca, 'XTickLabel', {'incorrect', 'correct'});
+ylabel('1 - max(\mu)');
+title('Uncertainty as 1-max(mu)');
+grid on;
+
+
+figure;
+boxplot(1 - muGap, yHat==y);
+set(gca, 'XTickLabel', {'incorrect', 'correct'});
+ylabel('1 - (largest(\mu) - second_largest(mu))');
+title('Uncertainty as 1 - muGap');
+grid on;
+
+
+figure;
+boxplot(variance, yHat==y);
+set(gca, 'XTickLabel', {'incorrect', 'correct'});
+ylabel('var');
+title('Uncertainty as var from predicted class');
+grid on;
 
 
 
 %-------------------------------------------------------------------------------
 % Visualize a few covariance matrices
 %-------------------------------------------------------------------------------
-for ii = 1:20
+for ii = 1:2
     Xi = squeeze(Prob(ii,:,:));   %  (#classes x #samples)
     Xi = Xi';                     %  -> rows-as-examples
     Cov = cov(Xi);
@@ -57,4 +101,6 @@ for ii = 1:20
     imagesc(Cov); colorbar;
     set(gca, 'YTick', 1:10, 'YTickLabel', classes);
     set(gca, 'XTick', 1:10, 'XTickLabel', classes);
+
+    pause()
 end
