09990923ed758231f3945b57ef77fff2b49df3f3
mike pekala <mpekala@umd.edu>
2015-11-20 16:26:16 -0500

    added

---
+++ src/augment_cifar10_lmdb.py
@@ -0,0 +1,101 @@
+"""Performs synthetic data augmentation on a CIFAR-10 LMDB database
+   (like the one provided as an example with Caffe)
+
+   XXX: should probably do something about correcting the mean vector...
+
+   Example (change paths as appropriate for your caffe):
+
+     PYTHONPATH=~/Apps/caffe/python python augment_cifar10_lmdb.py  ~/Apps/caffe/examples/cifar10/cifar10_train_lmdb  ./cifar10_train_lmdb_aug
+"""
+
+__author__ = "Mike Pekala"
+__license__ = "Apache 2.0"
+
+
+
+import sys, argparse, os.path, time
+import numpy as np
+import lmdb
+import pdb
+
+import caffe
+
+
+NAME='aug_cifar'
+
+
+def augment(X, maxJitter=2, hflip=True):
+    if hflip:
+        Xout = np.fliplr(X)
+    else: 
+        Xout = np.zeros(X.shape, dtype=X.dtype)
+        Xout[...] = X[...]
+
+    dx = np.round(maxJitter - np.random.rand() * 2 * maxJitter)
+    dy = np.round(maxJitter - np.random.rand() * 2 * maxJitter)
+
+    pdb.set_trace() # TEMP
+    Xout = np.roll(Xout, dx, axis=1)
+    Xout = np.roll(Xout, dy, axis=2)
+    return Xout
+
+
+
+def main(inDir, outDir, nAugment=3):
+
+    # load the data volumes (EM image and labels, if any)
+    print('[%s]: loading data from: %s' % (NAME, inDir))
+
+    # read in the entire data set.
+    X = [];  y = [];
+    datum = caffe.proto.caffe_pb2.Datum()
+    env = lmdb.open(inDir, readonly=True)
+    with env.begin() as txn:
+        cursor = txn.cursor()
+        for key, value in cursor:
+            datum.ParseFromString(value)
+            xv = np.fromstring(datum.data, dtype=np.uint8)
+            X.append(xv.reshape(datum.channels, datum.height, datum.width))
+            y.append(datum.label)
+    env.close()
+
+    # create a synthetic data set
+    print('[%s]: creating synthetic data...' % NAME)
+    idx = 0
+    datum = caffe.proto.caffe_pb2.Datum()
+    env = lmdb.open(outDir, map_size=X[0].size*len(X)*(nAugment+1))
+    with env.begin(write=True) as txn:
+        for ii in range(len(y)):
+            Xi = X[ii];  yi = y[ii]
+            datum.channels = Xi.shape[0]
+            datum.height = Xi.shape[1]
+            datum.width = Xi.shape[2]
+            datum.label = yi
+            datum.data = Xi.tostring()
+            strId = '{:08}'.format(idx)
+
+            txn.put(strId.encode('ascii'), datum.SerializeToString())
+            idx += 1
+
+            for jj in range(nAugment):
+                Xj = augment(Xi, hflip=np.mod(jj,2)==0) 
+                datum.data = Xj.tostring()
+                txn.put(strId.encode('ascii'), datum.SerializeToString()) 
+                idx += 1
+
+    return 
+
+
+if __name__ == "__main__":
+    inDir = sys.argv[1]
+    if not os.path.exists(inDir):
+        raise RuntimeError('Input directory %s does not exist!' % inDir)
+
+    outDir = sys.argv[2]
+    if os.path.exists(outDir):
+        raise RuntimeError('Output directory %s already exists; please move out of the way first' % outDir)
+
+    main(inDir, outDir)
+
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
